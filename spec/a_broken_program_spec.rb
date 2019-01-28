8381e31ddf5bd353bc3561a7a189dd6ace33475b
Ricardo H <ricardo.hudson923@gmail.com>
2019-01-28 13:18:49 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -3,7 +3,7 @@ describe 'A broken program' do
   it 'to not raises an error when loaded' do
     expect{
       load 'lib/a_broken_program.rb'
-    }.to_not raise_error
+    }.to raise_error
   end
 
 end
